36e82a0ab6f99eb1752e2d11c885f9f5fbb524ce
stetim94 <stevenkoffijberg@gmail.com>
2015-04-20 12:11:02 +0200

    solved mario

--- pset1/mario.c
+++ pset1/mario.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
 
 int main(void){
-	int i, j;
+	int i, j, k;
+	// get user input, value between 1 and 23
 	do {
 		printf("give me a number\n");
 		scanf("%i",&i); 
-	} while (i < 8 || i > 23);
+	} while (i < 1 || i > 23);
+	// j counts the number of rows of the pyramid
 	for (j = 0; j < i; j++){
-		printf("#\n");
+		// k prints out the required spaces
+		for (k = 1; k < (i - j); k++){
+			printf(" ");
+		}
+		// k prints out the number of hashes
+		for (k = 0; k < ( j + 1); k++ ){
+			printf("#");
+		}
+	printf("#\n");
 	}
 }
